0d8392aaa822fc3cbc607dbdb0eb7986c05e6d66
Silvi Sharma <47741827+silvisharma@users.noreply.github.com>
2020-10-03 01:42:13 +0530

    Add files via upload

---
+++ FibonacciExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class FibonacciExample1
+{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=0;i<count;i++)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+}
+}  
